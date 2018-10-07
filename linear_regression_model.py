9ed1b22b9590c4a59576ac7dd88cbc79218b3d19
Nicklaus Njihia <njihianicklas55@gmail.com>
2018-10-07 11:40:48 +0300

    Update

--- linear_regression_model.py
+++ linear_regression_model.py
@@ -37,7 +37,7 @@ with g.as_default():
                 print(step, sess.run([w,b]))
                 wb_.append(sess.run([w,b]))
         
-        print(10,sess.run(wb_))
+        
         print(10,sess.run([w,b]))
 
 
